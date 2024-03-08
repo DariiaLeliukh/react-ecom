f21bcef04084db68f871d5f371f851cb792fa434
DariiaLeliukh <dariialelyukh@gmail.com>
2024-03-07 19:59:09 -0800

    Add basic navigation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./index.css";
+import NavBar from "./components/NavBar";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <NavBar />
+      </>
+    );
+  }
+}
+
+export default App;
